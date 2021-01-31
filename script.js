893e0e83db8331df3b4c821181c47f9c5c65b7b7
Yuu Chin <shontzuforever@gmail.com>
2021-01-31 17:27:13 +0800

    Gameplay drawer

--- script.js
+++ script.js
@@ -254,3 +254,11 @@ function checkWin() {
 		}
 	}
 }
+
+function openDrawer() {
+	document.getElementById("drawer").style.width = "400px";
+  }
+  
+  function closeDrawer() {
+	document.getElementById("drawer").style.width = "0";
+  }
